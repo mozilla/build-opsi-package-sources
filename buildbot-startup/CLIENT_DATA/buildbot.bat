d3898bb99bfc5da40ff533a89d4e4732231f4f30
Dustin J. Mitchell <dustin@mozilla.com>
2011-05-19 12:25:14 -0500

    Bug 658263: sleep after START /MIN so as not to SIGBREAK the started process; r=bhearsum

--- buildbot-startup/CLIENT_DATA/buildbot.bat
+++ buildbot-startup/CLIENT_DATA/buildbot.bat
@@ -4,3 +4,6 @@ D:\mozilla-build\msys\bin\sleep.exe 30
 d:
 cd \mozilla-build
 start /min start-buildbot.bat
+
+REM and don't close this window so quickly that a SIGBREAK will be sent to start-buildbot.bat
+D:\mozilla-build\msys\bin\sleep.exe 30
