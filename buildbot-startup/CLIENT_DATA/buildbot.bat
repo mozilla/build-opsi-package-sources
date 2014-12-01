2d3f79b74a86b1c0724a5f90c0820e305ff714a1
Justin Wood <Callek@gmail.com>
2014-12-01 05:52:51 -0500

    Really Blank Repo

--- buildbot-startup/CLIENT_DATA/buildbot.bat
+++
@@ -1,9 +0,0 @@
-REM take some time to warm up
-D:\mozilla-build\msys\bin\sleep.exe 30
-
-d:
-cd \mozilla-build
-start /min start-buildbot.bat
-
-REM and don't close this window so quickly that a SIGBREAK will be sent to start-buildbot.bat
-D:\mozilla-build\msys\bin\sleep.exe 30
